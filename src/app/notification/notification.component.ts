26fa029e7e21f54096925c55d3bab28f7abb9120
Dhanush V D Upadya <dhanushupadya@gmail.com>
2022-12-09 14:45:04 +0530

    Made use of Property Binding to bring data from component to view

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-notification',
-  template: `<div class="alert alert-success"> 
+  template: `<div class="alert alert-success" [hidden]="displayNotification"> 
                <p> This website uses cookies to provide better user experience.</p>
             
               </div>`,
@@ -12,4 +12,5 @@ import { Component } from '@angular/core';
 })
 export class NotificationComponent {
 
+  displayNotification:boolean=true;
 }
