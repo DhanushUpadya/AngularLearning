bef27a6493f5d7cc25f72170005d37f697f93d3b
Dhanush V D Upadya <dhanushupadya@gmail.com>
2022-12-09 12:17:21 +0530

    Added multiple components via CLI and manually

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ConatinerComponent } from './Container/container.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { NotificationComponent } from './notification/notification.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConatinerComponent,
+    NavbarComponent,
+    HeaderComponent,
+    NotificationComponent
   ],
   imports: [
     BrowserModule
